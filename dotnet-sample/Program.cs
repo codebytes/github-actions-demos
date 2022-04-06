c43086ce8bd880ca80d4fdda9d0fe420aae57e03
Chris Ayers <clayers@gmail.com>
2022-04-06 15:23:34 -0700

    updated message

--- dotnet-sample/Program.cs
+++ dotnet-sample/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Dev Intersections!");
+app.MapGet("/", () => "Hello DevIntersections!");
 
 app.Run();
