7eea19d6be2f1c5c06a7d43f3de3b7d69b1e9fd1
Chris Ayers <clayers@gmail.com>
2023-12-02 10:06:44 -0500

    updated dotnet

--- dotnet-sample/Program.cs
+++ dotnet-sample/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from PowerShell + DevOps Summit!");
+app.MapGet("/", () => "Hello Granite State Code Camp!");
 
 app.Run();
