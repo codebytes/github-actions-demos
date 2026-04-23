7fbbeb06add3f1b947131e53f45d6ab1303281fc
Chris Ayers <clayers@gmail.com>
2026-04-24 09:17:27 +1000

    Update project files to target .NET 10.0 and add configuration for new projects

--- dotnet-sample/src/Program.cs
+++ dotnet-sample/src/Program.cs
@@ -4,3 +4,6 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello PowerShell + DevOps Summit!");
 
 app.Run();
+
+// Make Program accessible for integration tests (WebApplicationFactory<Program>)
+public partial class Program { }
